eb6c8f6a771ec760bb03ceb01b1c8ffb3ee5d93c
Santanu-2004 <146585281+Santanu-2004@users.noreply.github.com>
2024-07-16 15:58:07 +0000

    successfully  completed

---
+++ 01_DataType/Activity-2.js
@@ -0,0 +1,6 @@
+/* Constant Decleration */
+
+// Task 1: declear a variable using const, assign it a boolean value, and log the value to the console
+const userName = true
+console.log(userName);
+console.log(typeof userName);
---
+++ 01_DataType/Activity-3.js
@@ -0,0 +1,27 @@
+//creat variables of different data types(number, string, boolean,array, object) and log each variables type using the typeof operator
+
+//number-->
+const numberExmp = 45
+console.log(typeof numberExmp);
+
+//string-->
+const stringExmp = "Hello Viewer"
+console.log(typeof stringExmp);
+
+//boolean-->
+let boolExmp = Boolean (5)
+console.log(boolExmp);
+console.log(typeof boolExmp);
+
+//object-->
+let objExp =new  Object
+console.log(typeof objExp);
+
+//array-->
+const arrExp= new Array(4,5,6)
+console.log(arrExp);
+console.log(typeof arrExp);
+
+const myArr = [0, 1, 2, 8, 4, 5]                
+console.log(myArr[1]); 
+console.log(typeof myArr);
