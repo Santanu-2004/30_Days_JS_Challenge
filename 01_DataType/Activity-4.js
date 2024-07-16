92810c28a09ce8e322994bb5884031f2ac9ab892
Santanu-2004 <146585281+Santanu-2004@users.noreply.github.com>
2024-07-16 16:14:54 +0000

    done successfully

---
+++ 01_DataType/Activity-4.js
@@ -0,0 +1,9 @@
+//declear a variable using let, assign it an initial value, reassign a new value, and log both values to the console
+
+//type1-->
+let value1 = 58
+console.log(value1);
+
+value1 = 69
+console.log(value1);
+
---
+++ 01_DataType/Activity-5.js
@@ -0,0 +1,9 @@
+//try reassigning a variable decleared with const and observe the error
+
+const name = "Santanu"
+console.log(name);
+
+name = "Santanu Maiti"
+console.log(name);
+
+//this will provide A error as const value is constant & non changable
