80542dee814b74d9c0098d7b333f52d7795fdb95
Santanu-2004 <146585281+Santanu-2004@users.noreply.github.com>
2024-07-16 15:12:33 +0000

    first activity complete successfully

---
+++ 01_DataType/Activity-1.js
@@ -0,0 +1,12 @@
+/* Variable Decleration */
+
+// Task 1: declear a variable using var, assign it a number, and log the value to the console
+var FisrtNumber = 65
+console.log(FisrtNumber);
+console.log(typeof FisrtNumber);
+
+
+// Task 1: declear a variable using let, assign it a string, and log the value to the console
+let user = "Santanu Maiti"
+console.log(user);
+console.log(typeof user);
